f3c135490d14afb4bf0793d8a3167bd45dcefed2
Sufi-to <3thomas225@gmail.com>
2023-11-10 09:06:49 +0400

    test files added

--- littlelemon/restaurant/tests/test_models.py
+++ littlelemon/restaurant/tests/test_models.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
 from restaurant.models import MenuItem, Booking
+from decimal import Decimal
 
-class MenuItemTestCase(TestCase):
+class MenuItemTest(TestCase):
     def test_get_item(self):
         item = MenuItem.objects.create(title='IceCream', price=80, inventory=100)
         itemstr = item.get_item()
         self.assertEqual(itemstr, "IceCream:80")
 
-
-# class BookingTest(TestCase):
-#     def test_get_item(self):
-#         pass
+    def test_get_menu_item(self) -> None:
+        item = MenuItem.objects.get(id = self.item1.id)
+        self.assertEqual(item.title, 'Honey')
+        self.assertEqual(item.price, Decimal(13.00).quantize(Decimal("0.00")))
+        self.assertEqual(item.inventory, 100)
